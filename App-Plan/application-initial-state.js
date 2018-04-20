337fbd62069b068e4d3b58d7e862fd703c61018b
Pranav Kural <pranavkural@gmail.com>
2018-04-20 09:54:47 +0000

    Mock up of application's initial state

---
+++ App-Plan/application-initial-state.js
@@ -0,0 +1,10 @@
+let initialState = {
+    calcOpSelected: "sub", //calculation operator selected
+    prevNum: 17, // if any number typed previously
+    num: 2, // last pressed number digit
+    symbOnDis: 2, //symbol on display,
+    nextIsNum: true, // user is going to select a number (operator just selected)
+    currentHis: [], //history of the ongoing calculation,
+    completeHis: [], // history for the complete one user session
+}
+
