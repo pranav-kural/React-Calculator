ce90679802d8cc6a2acd3a2ffb2c1a71def34eea
Pranav Kural <pranavkural@gmail.com>
2018-05-04 21:46:44 +0000

    Added the dotPresentInNum property to state

--- App-plan/application-initial-state.js
+++ App-plan/application-initial-state.js
@@ -3,7 +3,7 @@ let initialState = {
     prevNum: 17, // if any number typed previously
     num: 2, // last pressed number digit
     symbOnDis: 2, //symbol on display,
-    nextIsNum: true, // user is going to select a number (operator just selected)
+    dotPresentInNum: false, // true is there is a dot present in the number being typed
     currentHis: [], //history of the ongoing calculation,
     completeHis: [], // history for the complete one user session
 }
