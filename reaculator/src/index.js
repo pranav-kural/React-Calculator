1da126531013954a7a10ad6e244c1b98fb3b5873
Pranav Kural <pranavkural042@gmail.com>
2018-05-08 10:27:23 +0000

    Testing completed for store backend

--- reaculator/src/index.js
+++ reaculator/src/index.js
@@ -4,14 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import storeFactory from './Store'
-import { addNumToCalc } from './Actions/ActionsCreators';
+import StoreTest from './Test/store-test'
 
-const store = storeFactory()
-store.dispatch(
-    addNumToCalc("3")
-)
+console.log(StoreTest())
 
+alert("running")
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
